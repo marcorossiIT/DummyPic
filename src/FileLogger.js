e1b48acb0ba5a0c3adab43d147f01e9a8552f808
Marcorossi <marco.rossi@aurealink.com>
2022-10-24 16:47:40 +0200

    production only prints on console: every log is redirected to cosole in production issue#1

--- src/FileLogger.js
+++ src/FileLogger.js
@@ -1,22 +1,27 @@
+
 module.exports = {
     outlog: function (...args) {
-        const logpath = "logs/" // path riferita al punto di avvio del programma
-        fs = require('fs');
-        let dataOggiArr = new Date().toISOString().split('T');
-        let oggi = dataOggiArr[0]
-        args.forEach(arg => {
-            try {
-                let oraoggiArr = dataOggiArr[1].split('.');
-                let secs = oraoggiArr[0]
-                let mills = oraoggiArr[1]
+        if (process.env.env === 'prod') {
+            console.log(...args);
+        } else if (process.env.env === 'dev') {
+            const logpath = "logs/" // path riferita al punto di avvio del programma
+            fs = require('fs');
+            let dataOggiArr = new Date().toISOString().split('T');
+            let oggi = dataOggiArr[0]
+            args.forEach(arg => {
+                try {
+                    let oraoggiArr = dataOggiArr[1].split('.');
+                    let secs = oraoggiArr[0]
+                    let mills = oraoggiArr[1]
 
-                fs.appendFileSync(`${logpath}/${oggi}.log`,
-                    '[O | ' + `${secs} ${mills}` + ' ] ' + (typeof arg === 'string' ? arg : JSON.stringify(arg)) + "\n",
-                    { encoding:'utf8'})
-            } catch (e) {
-                console.error('outlog fails: ', e)
-            }
-        });
+                    fs.appendFileSync(`${logpath}/${oggi}.log`,
+                        '[O | ' + `${secs} ${mills}` + ' ] ' + (typeof arg === 'string' ? arg : JSON.stringify(arg)) + "\n",
+                        { encoding: 'utf8' })
+                } catch (e) {
+                    console.error('outlog fails: ', e)
+                }
+            });
+        }
     }
 };
 
